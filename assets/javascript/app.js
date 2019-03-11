04ac9f4993bd795246f0de8ce0479fa64f1c00fe
SNORRISS <samuel.norris@mu.edu>
2019-03-10 20:52:42 -0500

    main functionality completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,10 @@ var questions = [];
 var queryURL =
   "https://opentdb.com/api.php?amount=50&category=15&type=multiple";
 
+var time = 10;
+var timeEnd = false;
+var answers = [];
+
 $.ajax({
   url: queryURL,
   method: "GET"
@@ -9,10 +13,68 @@ $.ajax({
   questions = response.results;
   console.log(questions);
 
-  divCreate();
-  divCreate();
-  divCreate();
+  divCreate(1);
+  divCreate(2);
+  divCreate(3);
+  start();
 });
+
+$(document).on("click", "input", function(){
+
+
+console.log("test");
+var group = $(this).attr("class");
+console.log(group);
+
+ $("." + group).prop("checked", false);
+ $(this).prop("checked", true);
+
+if($(this).attr("correct") == "true"){
+answers[group] = true;
+console.log(answers[group]);
+}else{
+  answers[group] = false;
+  console.log(answers[group]);
+
+}
+})
+
+function start(){
+intervalid = setInterval(count, 1000);
+
+}
+
+function stop(){
+clearInterval(intervalid);
+}
+
+function count(){
+
+time--;
+$("#timer").text("Time Remaining: " + time + " seconds");
+if(time < 0){
+  timeEnd = true;
+  stop();
+  endScreen();
+}
+
+}
+
+function endScreen(){
+$("#card").html("");
+
+for(var i = 1; i < 4; i++){
+  var newDiv = $("<div>");
+  if(answers[i] == true){
+  newDiv.html("Question "+ i + ": Correct");
+  }else{
+    newDiv.html("Question "+ i + ": Incorrect");
+
+  }
+  $("#card").append(newDiv);
+}
+
+}
 
 function getRandomInt() {
   var max = Math.floor(questions.length);
@@ -26,7 +88,7 @@ function getRandom(max, min) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function divCreate() {
+function divCreate(group) {
   var newDiv = $("<div>");
   var newH2 = $("<h2>");
   var num = getRandomInt();
@@ -72,6 +134,13 @@ function divCreate() {
         .replace(/&eacute;/g, "é")
     );
 
+    if(ansArray[rand] == questions[num].correct_answer){
+      newRadio.attr("correct", "true");
+    }else{
+      newRadio.attr("correct", "false");
+    }
+
+    newRadio.attr("class", group);
     ansArray.splice(rand, 1);
     newAnswers.append(newRadio);
     newAnswers.append(newLabel);
